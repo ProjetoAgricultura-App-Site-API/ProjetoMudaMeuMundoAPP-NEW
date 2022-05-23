fc61efb69e7c4ed45043f1ccec27028cfb8b217a
Ergo <sergio5203@gmail.com>
2022-05-23 12:43:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Muda Meu Mundo',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
